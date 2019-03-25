b15492a3097a4f1b7a6e97fd7fd82c12cb1268ce
Isaac Kendall <isaac@isaac-kendall.com>
2019-03-24 17:20:58 -0700

    removed some folders

---
+++ examples/A0_to_ThingSpeak/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "thecottage"                                // replace MySSID with your WiFi network name
+#define SECRET_PASS "BadRomance324"                            // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 739746                                 // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "KEHLDB603ZSTFGRF"                           // replace XYZ with your channel write API Key
